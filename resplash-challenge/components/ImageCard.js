4fb41f3936918e32ccde147d259b596db34d1434
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2022-07-27 17:23:29 -0400

    Import card and list components to index.js

---
+++ resplash-challenge/components/ImageCard.js
@@ -0,0 +1,5 @@
+import styles from '../styles/ImageCard.module.css';
+
+const ImageCard = () => <div className={styles.image}></div>;
+
+export default ImageCard;
---
+++ resplash-challenge/components/ImageList.js
@@ -0,0 +1,5 @@
+import styles from '../styles/ImageList.module.css';
+
+const ImageList = () => <div className={styles.imageList}></div>;
+
+export default ImageList;
